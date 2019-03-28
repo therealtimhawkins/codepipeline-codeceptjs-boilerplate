193f0be69628baec6aae88d6c1a228241ededf88
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-28 20:59:22 +0000

    adds test for wrong postcode alert

--- e2eTests/homepage_test.js
+++ e2eTests/homepage_test.js
@@ -7,8 +7,13 @@ Scenario('Homepage should load on "/"', I => {
 
 Scenario('Error message should load when no postcode given', I => {
   I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
-  I.seeElement('.searchBarSubmitButton');
   I.click('.searchBarSubmitButton');
-  I.seeElement('.alertBox');
   I.see('FILL IN YER POSTCODE!', '.alertText');
+});
+
+Scenario('Error message should flash when a postcode with no results is given', I => {
+  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
+  I.fillField('.searchBarTextInput', 'testingpostcode');
+  I.click('.searchBarSubmitButton');
+  I.see('NO RESTAURANTS FOR YOU!', '.alertText');
 });
