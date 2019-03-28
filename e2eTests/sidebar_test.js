6f91073edcef87de2dc49a74486e78e72549e0a4
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-28 21:44:27 +0000

    adds sidebar test for home link

--- e2eTests/sidebar_test.js
+++ e2eTests/sidebar_test.js
@@ -7,3 +7,13 @@ Scenario('SideBar should open on click of burger button.', I => {
   I.waitForElement('.bm-item-list')
   I.see('HOME', '.bm-item-list');
 });
+
+Scenario('SideBar "HOME" Link should route to "/"', I => {
+  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/restaurants');
+  I.waitForElement('.logo', 10);
+  I.click('.bm-burger-button');
+  I.waitForElement('.bm-item-list')
+  I.see('HOME', '.bm-item-list');
+  I.click('HOME', '.bm-item-list'); 
+  I.dontSeeInCurrentUrl("/restaurants");
+});
