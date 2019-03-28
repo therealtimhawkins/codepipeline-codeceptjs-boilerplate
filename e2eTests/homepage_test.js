3e0eb6186a773d4bb6cc1338c7526c511ed8946f
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-03-28 20:56:38 +0000

    add test for no postcode given

--- e2eTests/homepage_test.js
+++ e2eTests/homepage_test.js
@@ -1,6 +1,14 @@
 Feature('Homepage');
 
-Scenario('navigate to homepage', I => {
+Scenario('Homepage should load on "/"', I => {
   I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
-  I.saveScreenshot('frontpageScreenshot.png');
+  I.seeElement('.logo');
+});
+
+Scenario('Error message should load when no postcode given', I => {
+  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
+  I.seeElement('.searchBarSubmitButton');
+  I.click('.searchBarSubmitButton');
+  I.seeElement('.alertBox');
+  I.see('FILL IN YER POSTCODE!', '.alertText');
 });
