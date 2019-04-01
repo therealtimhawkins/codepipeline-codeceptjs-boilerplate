499c3d811eecd179e0635f64672114b41aca8d2e
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2019-04-01 16:30:28 +0100

    initial commit of codecept boiler plate

--- e2eTests/homepage_test.js
+++ e2eTests/homepage_test.js
@@ -1,27 +1,5 @@
-Feature('Homepage');
+Feature('Example Homepage Test');
 
 Scenario('Homepage should load on "/"', I => {
-  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
-  I.seeElement('.logo');
-});
-
-Scenario('Error message should load when no postcode given', I => {
-  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
-  I.click('.searchBarSubmitButton');
-  I.see('FILL IN YER POSTCODE!', '.alertText');
-});
-
-Scenario('Error message should flash when a postcode with no results is given', I => {
-  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
-  I.fillField('.searchBarTextInput', 'testingpostcode');
-  I.click('.searchBarSubmitButton');
-  I.waitForElement('//*[@id="root"]/div/div[2]/div[2]/div/div/div', 10);
-});
-
-Scenario('Should route to /restaurants if results are found...', I => {
-  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/');
-  I.fillField('.searchBarTextInput', 'E147DX');
-  I.click('.searchBarSubmitButton');
-  I.waitForElement('.restaurantTile', 10);
-  I.seeInCurrentUrl("/restaurants");
+  I.amOnPage(process.env.UrlToTest);
 });
--- e2eTests/restaurantpage_test.js
+++
@@ -1,7 +0,0 @@
-Feature('Homepage');
-
-Scenario('Should be routed to home page if there is not restaurant data', I => {
-  I.amOnPage('http://reactuiservice.eu-west-2.elasticbeanstalk.com/restaurants');
-  I.waitForElement('.logo', 10);
-  I.dontSeeInCurrentUrl("/restaurants");
-});
