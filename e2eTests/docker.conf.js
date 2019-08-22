f154c9f2757e9e383012262bd97b070212e57ca6
therealtimhawkins <timothyhaydenhawkins@gmail.com>
2019-08-22 09:59:45 +0100

    adds docker scripts

--- e2eTests/docker.conf.js
+++ e2eTests/docker.conf.js
@@ -3,28 +3,25 @@ exports.config = {
   output: './output',
   helpers: {
     WebDriver: {
-      url: 'http://codeceptio-container:3000',
-      host: 'chrome-container',
+      url: 'https://trade.cazana.com',
+      host: 'selenium-ch',
       browser: 'chrome',
       smartWait: 5000,
       restart: false,
       waitForTimeout: 10000,
       timeouts: {
         script: 60000,
-        'page load': 60000,
+        'page load': 60000
       },
       coloredLogs: true,
       desiredCapabilities: {
         chromeOptions: {
-          args: [
-            '--headless',
-            '--disable-gpu', 
-          ],
-        },
-      },
-    },
+          args: ['--headless', '--disable-gpu']
+        }
+      }
+    }
   },
   bootstrap: false,
   mocha: {},
-  name: 'codeceptjs-docker',
-};
+  name: 'codeceptjs-docker'
+}
